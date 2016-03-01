ac86e009e623d7bb2ee9f03bbb01eecd9fb303d6
Niclas Darville <contact@ndarville.com>
2016-03-01 12:56:10 +0100

    Fixes #2 by adding perfect tense

--- src/content.js
+++ src/content.js
@@ -24,6 +24,7 @@ function handleText(textNode) {
 function replaceText(v) {
     var verbLo = "beskære", // Infinitive
         verbHi = "Beskære", // Infinitive
+        verbPerfLo = "beskåret",
         nounLo = "nedskæring",
         nounHi = "Nedskæring",
         uberNounLo = "dummebøde",
@@ -36,6 +37,7 @@ function replaceText(v) {
     // Effektivisering
     v = v.replace(/\beffektivisering\b/g, nounLo);
     v = v.replace(/\bEffektivisering\b/g, nounHi);
+    v = v.replace(/\beffektiviseret\b/g, verbPerfLo);
     v = v.replace(/\beffektivisere\b/g, verbLo);
     v = v.replace(/\bEffektivisere\b/g, verbHi);
 
@@ -51,6 +53,7 @@ function replaceText(v) {
 
     v = v.replace(/\bomprioritering\b/g, nounLo);
     v = v.replace(/\bOmprioritering\b/g, nounHi);
+    v = v.replace(/\bomprioriteret\b/g, verbPerLo);
     v = v.replace(/\bomprioritere\b/g, verbLo);
     v = v.replace(/\bOmprioritere\b/g, verbHi);
 
@@ -61,6 +64,7 @@ function replaceText(v) {
     // Normalisering
     v = v.replace(/\bnormalisering\b/g, nounLo);
     v = v.replace(/\bNormalisering\b/g, nounHi);
+    v = v.replace(/\bnormaliseret\b/g, verbPerfLo);
     v = v.replace(/\bnormalisere\b/g, verbLo);
     v = v.replace(/\bNormalisere\b/g, verbHi);
 
